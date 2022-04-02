6c060a6b8eefa79ea1658ce751e884815f23cb10
Ashley Grenon <ashley.grenon@gmail.com>
2022-04-02 18:08:37 -0500

    More stuff

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('boilerplate');
+import Phaser from './node_modules/src/phaser.js';
+
+const config = {
+    width: 800,
+    height: 600,
+    type: Phaser.AUTO
+}
+
+const game = new Phaser.Game(config);
